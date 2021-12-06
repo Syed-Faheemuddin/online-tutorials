223c0f30f64b4acf9e6e56112c48333ecaeab4f5
Syed Faheemuddin <syedfaheemuddin@gmail.com>
2021-12-06 21:16:39 +0530

    first commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,22 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="#"><i class="fa fa-files-o"></i> Courses</a>
+					<ul>
+						<li><a href="add-courses.php">Add Courses</a></li>
+						<li><a href="manage-courses.php">Manage Courses</a></li>
+					</ul>
+				 
+				<li><a href="#"><i class="fa fa-files-o"></i>Tutorials</a> 
+					<ul>
+						<li><a href="add-vids.php">Add Videos</a></li>
+						<li><a href="videos.php">Available Videos</a></li>
+					</ul>
+				</li>	
+ 				<li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Students</a></li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
---
+++ videos.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+include('includes/config.php');
+include('includes/checklogin.php');
+check_login();
+?>
+
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	<title>Manage Rooms</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<link rel="stylesheet" href="css/style.css">
+<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+ if(popUpWin)
+{
+if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=yes,width='+510+',height='+430+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+</script>
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+
+	<div class="ts-main-content">
+			<?php include('includes/sidebar.php');?>
+            
+            <div class="content-wrapper">
+			<div class="container-fluid">     
+            <br>
+            <h2>Available Videos :</h2>
+<?php	
+$aid=$_SESSION['id'];
+$ret="select * from videos";
+$stmt= $mysqli->prepare($ret) ;
+//$stmt->bind_param('i',$aid);
+$stmt->execute() ;//ok
+$res=$stmt->get_result();
+$cnt=1;
+while($row=$res->fetch_object())
+	  {
+	  	?>
+            <div class="row">
+             <div class="col-md-12">          
+<p>  Title : <b><?php echo $row->TITLE;?><b> <br>
+<?php echo $row->DESCRIPTION ;?><br> Course Code : <?php echo $row->COURSE_CODE;?> 
+<br>
+<iframe width="560" height="315" src="<?php echo $row->URL?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                
+<br>
+<?php
+$cnt=$cnt+1;
+	} ?>
+    
+    <br>
+    
+
+	<!-- Loading Scripts -->
+ 	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
